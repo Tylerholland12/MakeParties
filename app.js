56ea5bd1ae45855416607f641f5f7185962dced6
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-12-14 12:08:30 -0600

    [first commit]

---
+++ app.js
@@ -0,0 +1,26 @@
+// Initialize express
+const express = require('express')
+const app = express()
+
+// require handlebars
+const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+
+// Use "main" as our default layout
+app.engine('handlebars', exphbs({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
+// Use handlebars to render
+app.set('view engine', 'handlebars');
+
+// Tell our app to send the "hello world" message to our home page
+app.get('/', (req, res) => {
+    res.render('home', { msg: 'Handlebars are Cool!' });
+})
+
+// Choose a port to listen on
+const port = process.env.PORT || 3000;
+
+// Tell the app what port to listen on
+app.listen(port, () => {
+  console.log('App listening on port 3000!')
+})
