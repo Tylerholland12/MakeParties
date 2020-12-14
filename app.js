c681ff97a4f4b0019e1dda0ae1d8a128c4039d6a
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-12-14 17:48:14 -0600

    [add && update] Users can edit and update events

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // Initialize express
 const express = require('express')
+const methodOverride = require('method-override')
+
 const app = express()
+
+// override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'))
 
 // require handlebars
 const exphbs = require('express-handlebars');
@@ -45,18 +50,54 @@ app.get('/events/new', (req, res) => {
   res.render('events-new', {});
 })
 
-app.post('/events', (req, res) => {
-  console.log(req.body);
-})
+// app.post('/events', (req, res) => {
+//   console.log(req.body);
+// })
 
 // CREATE
 app.post('/events', (req, res) => {
   models.Event.create(req.body).then(event => {
-    res.redirect(`/`);
+    // Redirect to events/:id
+    res.redirect(`/events/${event.id}`)
+
   }).catch((err) => {
     console.log(err)
   });
 })
+
+// SHOW
+app.get('/events/:id', (req, res) => {
+  // Search for the event by its id that was passed in via req.params
+  models.Event.findByPk(req.params.id).then((event) => {
+    // If the id is for a valid event, show it
+    res.render('events-show', { event: event })
+  }).catch((err) => {
+    // if they id was for an event not in our db, log an error
+    console.log(err.message);
+  })
+})
+
+// EDIT
+app.get('/events/:id/edit', (req, res) => {
+  models.Event.findByPk(req.params.id).then((event) => {
+    res.render('events-edit', { event: event });
+  }).catch((err) => {
+    console.log(err.message);
+  })
+});
+
+// UPDATE
+app.put('/events/:id', (req, res) => {
+  models.Event.findByPk(req.params.id).then(event => {
+    event.update(req.body).then(event => {
+      res.redirect(`/events/${req.params.id}`);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
+});
 
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
