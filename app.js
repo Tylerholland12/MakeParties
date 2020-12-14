8bd378b35a2992c68d6f0ac4c5fa9d406275afcd
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-12-14 16:14:36 -0600

    [add && update]

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars')
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
+
+const models = require('./db/models');
+
+
+// The following line must appear AFTER const app = express() and before your routes!
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Use "main" as our default layout
 app.engine('handlebars', exphbs({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
 // Use handlebars to render
@@ -24,10 +33,30 @@ var events = [
     { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
   ]
   
-  // INDEX
-  app.get('/events', (req, res) => {
+// Index
+app.get('/', (req, res) => {
+  models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
     res.render('events-index', { events: events });
   })
+})
+
+  // NEW
+app.get('/events/new', (req, res) => {
+  res.render('events-new', {});
+})
+
+app.post('/events', (req, res) => {
+  console.log(req.body);
+})
+
+// CREATE
+app.post('/events', (req, res) => {
+  models.Event.create(req.body).then(event => {
+    res.redirect(`/`);
+  }).catch((err) => {
+    console.log(err)
+  });
+})
 
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
