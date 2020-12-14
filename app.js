59cfa2d620e0c1373911696f5711eda434ed96ee
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-12-14 12:21:26 -0600

    [update && add] users can see events

--- app.js
+++ app.js
@@ -17,6 +17,18 @@ app.get('/', (req, res) => {
     res.render('home', { msg: 'Handlebars are Cool!' });
 })
 
+// OUR MOCK ARRAY OF PROJECTS
+var events = [
+    { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+    { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+    { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
+  ]
+  
+  // INDEX
+  app.get('/events', (req, res) => {
+    res.render('events-index', { events: events });
+  })
+
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
 
